e5b0af2da37a545a34056de8cdccebf85f8764be
Gabriel-Malenowitch <gabrielbotelhomalenowitch@gmail.com>
2025-03-09 21:41:31 -0300

    add dist

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,37 @@ function App() {
 
   return (
     <Stack alignItems={"center"} spacing={2} p={1}>
-      <img style={{ width: 200}} src="images/LOGO-BRANCA-SEM-FUNDO-e1717859020398-1024x355.webp" />
+      <img style={{ width: 200}} src="https://vencendoojogo.site/biblia-tube/images/LOGO-BRANCA-SEM-FUNDO-e1717859020398-1024x355.webp" />
       <Typography fontWeight={600}>APROXIME O SEU FILHO DE JESUS, ENSINANDO OS PRINCÍPIOS DA BÍBLIA</Typography>
-      <img style={{ width: 350}} src="images/Instagram-post-para-culto-infantil-divertido-colorido-7-1-e1736473927917-1024x696.png-1.webp" />
+      <img style={{ width: 350}} src="https://vencendoojogo.site/biblia-tube/images/Instagram-post-para-culto-infantil-divertido-colorido-7-1-e1736473927917-1024x696.png-1.webp" />
       <Typography fontWeight={600}>Experimente a Plataforma que Está Mudando a Maneira como as Famílias estão ensinando os <span className='red'>Valores Sagrados</span> aos seus filhos.</Typography>
       <Typography>Dê ao seu filho acesso a uma plataforma única, repleta de desenhos animados, músicas e conteúdos lúdicos que ensinam os valores e princípios bíblicos de forma divertida e envolvente. Aqui, o aprendizado da Palavra de Deus se transforma em momentos de alegria e crescimento, ajudando a moldar o comportamento em casa e na escola. Garanta um entretenimento seguro que não apenas diverte, mas também planta sementes eternas no coração do seu pequeno. Invista hoje no futuro espiritual e emocional do seu filho!</Typography>
 
       <Stack spacing={2} direction={"row"} className='carousel-container'>
-        <img className='cover' src="images/PRIMEIRA-1-768x960.webp" />
-        <img className='cover' src="images/SEGUNDA-2-768x960.webp" />
-        <img className='cover' src="images/TERCEIRA-2-768x960.webp" />
-        <img className='cover' src="images/QUARTA-2-768x960.webp" />
-        <img className='cover' src="images/QUINTA-2-768x960.webp" />
-        <img className='cover' src="images/SEXTA-2-768x960.webp" />
-        <img className='cover' src="images/SETIMA-2-768x960.webp" />
-        <img className='cover' src="images/OITAVA-2-1-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/PRIMEIRA-1-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/SEGUNDA-2-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/TERCEIRA-2-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/QUARTA-2-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/QUINTA-2-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/SEXTA-2-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/SETIMA-2-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/OITAVA-2-1-768x960.webp" />
       </Stack>
 
 
       <Stack spacing={2} direction={"row"} className='carousel-container'>
-        <img className='cover' src="images/Copia-de-Copia-de-Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-10-768x960.webp" />
-        <img className='cover' src="images/Copia-de-Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-9-768x960.webp" />
-        <img className='cover' src="images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-1-768x960.webp" />
-        <img className='cover' src="images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-11-768x960.webp" />
-        <img className='cover' src="images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-13-768x960.webp" />
-        <img className='cover' src="images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-768x960.webp" />
-        <img className='cover' src="images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-6-768x960.webp" />
-        <img className='cover' src="images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-7-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Copia-de-Copia-de-Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-10-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Copia-de-Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-9-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-1-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-11-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-13-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-6-768x960.webp" />
+        <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/Post-instagram-retrato-culto-de-santa-ceia-moderno-marrom-e-bege-7-768x960.webp" />
       </Stack>
 
     <Stack alignItems={"center"} spacing={2}>
-      <img style={{width: 200}} src="images/a-melhor-garantia-do-mercado-1.webp" alt="" />
+      <img style={{width: 200}} src="https://vencendoojogo.site/biblia-tube/images/a-melhor-garantia-do-mercado-1.webp" alt="" />
       <Typography fontSize={'1.9rem'} fontWeight={600}>
         Experimente gratuitamente por 7 dias!
       </Typography>
@@ -49,12 +49,12 @@ function App() {
       
       
     <Stack spacing={2} direction={"row"} className='carousel-container'>
-      <img className='cover' src="images/2-2-768x960.webp" />
-      <img className='cover' src="images/3-2-768x960.webp" />
-      <img className='cover' src="images/4-2-768x960.webp" />
-      <img className='cover' src="images/5-1-768x960.webp" />
-      <img className='cover' src="images/6-1-768x960.webp" />
-      <img className='cover' src="images/7-2-768x960.webp" />
+      <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/2-2-768x960.webp" />
+      <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/3-2-768x960.webp" />
+      <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/4-2-768x960.webp" />
+      <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/5-1-768x960.webp" />
+      <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/6-1-768x960.webp" />
+      <img className='cover' src="https://vencendoojogo.site/biblia-tube/images/7-2-768x960.webp" />
     </Stack>
     <Typography fontSize={'1.6rem'} fontWeight={600}>
       E Muito Mais...
@@ -74,16 +74,16 @@ function App() {
       </Typography>
         <br />
       <Stack spacing={2} alignItems={"center"}>
-        <img className='notice-cover' src="images/49-e1737058136398.webp" alt="" />
-        <img className='notice-cover' src="images/50-e1737058165324.webp" alt="" />
-        <img className='notice-cover'  src="images/51-e1737058191101.webp" />
-        <img className='notice-cover'  src="images/55-e1737058217267.webp" />
+        <img className='notice-cover' src="https://vencendoojogo.site/biblia-tube/images/49-e1737058136398.webp" alt="" />
+        <img className='notice-cover' src="https://vencendoojogo.site/biblia-tube/images/50-e1737058165324.webp" alt="" />
+        <img className='notice-cover'  src="https://vencendoojogo.site/biblia-tube/images/51-e1737058191101.webp" />
+        <img className='notice-cover'  src="https://vencendoojogo.site/biblia-tube/images/55-e1737058217267.webp" />
         <Typography fontSize={'1.6rem'} fontWeight={600}>
           “Ensine a criança no caminho em que deve andar, e, ainda quando for velho, não se desviará dele.” — Provérbios 22:6
         </Typography>
-        <img className='notice-cover' src="images/52_1-e1737056485555.webp" />
-        <img className='notice-cover' src="images/53-e1737056510181.webp" alt="" />
-        <img className='notice-cover' src="images/54-e1737056537284.webp" />
+        <img className='notice-cover' src="https://vencendoojogo.site/biblia-tube/images/52_1-e1737056485555.webp" />
+        <img className='notice-cover' src="https://vencendoojogo.site/biblia-tube/images/53-e1737056510181.webp" alt="" />
+        <img className='notice-cover' src="https://vencendoojogo.site/biblia-tube/images/54-e1737056537284.webp" />
 
       </Stack>
     </Box>
